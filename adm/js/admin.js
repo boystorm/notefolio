7f3b0bb2ac76276bd631171d1de2e367923eeb8a
boystorm <motokrzr11@gmail.com>
2022-02-02 16:36:55 +0900

    insert sucesss

--- adm/js/admin.js
+++ adm/js/admin.js
@@ -204,22 +204,22 @@ $(function() {
      * 설  명 : file uploader library
      * =======================================
      */   
-     $('#ssi-upload').ssi_uploader({
-        url: 'http://ssinput.com/php/upload.php'
-    });
+    //  $('#ssi-upload').ssi_uploader({
+    //     url: 'http://ssinput.com/php/upload.php'
+    // });
 
     /**
      * =======================================
      * 설  명 : smart editor2.0 library
      * =======================================
      */   
-    var oEditors = [];
+    // var oEditors = [];
     
-    nhn.husky.EZCreator.createInIFrame({
-        oAppRef: oEditors,
-        elPlaceHolder: "ir1",
-        sSkinURI: "../../lib/smarteditor2/SmartEditor2Skin.html",
-        fCreator: "createSEditor2"
-    });
+    // nhn.husky.EZCreator.createInIFrame({
+    //     oAppRef: oEditors,
+    //     elPlaceHolder: "ir1",
+    //     sSkinURI: "../../lib/smarteditor2/SmartEditor2Skin.html",
+    //     fCreator: "createSEditor2"
+    // });
 });
 
--- main.js
+++ main.js
@@ -4,14 +4,37 @@ const port = 3000;
 const db_config = require(__dirname + '/config/database.js');
 const conn = db_config.init();
 
+let qs = require('querystring');
+
+
 //adm 폴더의 클라이언트 접근 허용 (미들웨어) : 추가되어야 불러온 html 에서 css/javasciprt 경로를 제대로 잡음
 app.use(express.static(__dirname + "/adm"));
+app.use("/lib",express.static(__dirname + "/lib"));
 
 db_config.connect(conn);
 
 app.get('', (req, res) => {
   return res.sendFile(__dirname + '/adm/templates/memberEdit.html');
 })
+
+app.post('/create_process', function (req, res) {
+    let body = '';
+    req.on('data', function(data){
+        body = body + data;
+    });
+
+    req.on('end', function(){
+        let post = qs.parse(body);
+         console.log(post)
+        let sql = 'INSERT INTO AUTHOR (id, password) VALUES(?, ?)';
+        let params = [post.id, post.password];
+        conn.query(sql, params, function(err) {
+            if(err) console.log('query is not excuted. insert fail...\n' + err);
+            else res.redirect('/');
+        });
+        
+    })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
