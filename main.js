f7e55ce4172c85d3747527fa1e598a0b2a52b713
boystorm <motokrzr11@gmail.com>
2022-02-04 22:15:21 +0900

    password modify

--- main.js
+++ main.js
@@ -20,36 +20,72 @@ app.get('/adm', (req, res) => {
 
 // 로그인 아이디 패스워드 입력
 app.post('/login_process', function (req, res) {
-      let body = '';
-      req.on('data', function(data){
-          body = body + data;
-      });
-  
-      req.on('end', function(){
-          let post = qs.parse(body);
-          conn.query("SELECT * FROM AUTHOR", function (err, result, fields) {
-            if (err) {
-              throw err
-            } else {
-              if(result[0].id === post.id && parseInt(result[0].password) === parseInt(post.password)){
-                console.log("success");
-                // 쿠키 세션 ? 주면서 넘기기 확인
-                res.redirect("/manage");
-              } else {
-                console.log("fail");
-                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-                res.write("<script>alert('아이디 패스워드를 확인해 주세요')</script>");
-                res.write("<script>window.location=\"/adm\"</script>");
-              }
-            }
-          });
-      })
+  let body = '';
+  req.on('data', function(data){
+    body = body + data;
   });
+  
+  req.on('end', function(){
+     let post = qs.parse(body);
+     conn.query("SELECT * FROM AUTHOR", function (err, result, fields) {
+      if (err) {
+        throw err
+      } else {
+        if(result[0].id === post.id && parseInt(result[0].password) === parseInt(post.password)){
+          console.log("success");
+          // 쿠키 세션 ? 주면서 넘기기 확인
+          res.redirect("/manage");
+        } else {
+          console.log("fail");
+          res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+          res.write("<script>alert('아이디 패스워드를 확인해 주세요')</script>");
+          res.write("<script>window.location=\"/adm\"</script>");
+        }
+      }
+    });
+  })
+});
 
-// 로그인 화면 첫 로딩
+// 매니지 페이지
 app.get('/manage', (req, res) => {
   return res.sendFile(__dirname + '/adm/templates/manage.html');
 })
+
+// 패스워드 찾기
+app.get('/password', (req, res) => {
+  return res.sendFile(__dirname + '/adm/templates/password.html');
+})
+
+// 로그인 아이디 패스워드 입력
+app.post('/password_process', function (req, res) {
+  let body = '';
+  req.on('data', function(data){
+      body = body + data;
+  });
+  req.on('end', function(){
+    let post = qs.parse(body);
+    if(post.passwordHint === "yangbankim"){
+        conn.query("SELECT * FROM AUTHOR", function (err, result, fields) {
+          if (err) {
+            throw err
+          } else {
+            console.log("success")
+            // ajax 찾아보기
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});           
+            res.write("<script>window.location=\"/password\"</script>");
+            res.write("<script>alert('아이디 패스워드를 확인해 주세요')</script>");
+            console.log(result[0].password);
+          }
+        });
+    } else {
+      console.log("fail");
+      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+      res.write("<script>alert('힌트가 맞지 않습니다.')</script>");
+      res.write("<script>window.location=\"/password\"</script>");
+    }
+  });
+});
+
 
 
 // 등록, 수정 페이지
