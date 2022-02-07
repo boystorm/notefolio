80d4a5009605e89a87f408c31effd9ea555ebb8d
yjh6527 <motokrzr11@gmail.com>
2022-02-07 13:00:42 +0900

    query modify

--- example.sql
+++ example.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `author` (
-    `id` int(11) NOT NULL,
+    `id` varchar(11) NOT NULL,
     `password` int(9) NOT NULL,
     PRIMARY KEY (`id`) 
 );
 
-INSERT INTO `author` VALUES ('admin', '123456');
+INSERT INTO `author` (id, password) VALUES ('admin', '123456');
