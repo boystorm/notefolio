2a0de868768d327327e35a04f1a0b6623525dea4
yjh6527 <motokrzr11@gmail.com>
2022-02-09 17:59:11 +0900

    login modify

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const conn = db_config.init();
 db_config.connect(conn);
 // url 쿼리 문자열 
 let qs = require('querystring');
-var cookieParser = require('cookie-parser')
-app.use(cookieParser())
+// 쿠키 파서
+let cookieParser = require('cookie-parser')
+// 세션 파서
+let expressSession = require('express-session');
 
-// cookie-paser 호출도 안했는데 가능한지
-// session 호출없이도 가능할지
 
 /**
  * =======================================
@@ -28,6 +28,14 @@ app.use(cookieParser())
 // 정적 파일
 app.use(express.static(__dirname + "/adm"));
 app.use("/lib",express.static(__dirname + "/lib"));
+// 쿠키 파서
+app.use(cookieParser());
+// 세션 파서
+app.use(expressSession({
+    secret: 'my key',   // 쿠키를 임의로 변조하는 것을 막음
+    resave: false,       // 세션을 언제나 저장할지, false 권장
+    saveUninitialized: false     // 세션이 저장되기 전에 uninitialized 상태로 미리 만들어서 저장
+}));
 
 /**
  * =======================================
@@ -52,18 +60,18 @@ app.post('/login_process', function (req, res) {
   });
   
   req.on('end', function(){
-     let post = qs.parse(body);
-     conn.query("SELECT * FROM AUTHOR", function (err, result, fields) {
+    let post = qs.parse(body);
+
+    // id password let
+    let userid = post.id;
+    let password = post.password;
+
+    conn.query("SELECT * FROM author WHERE id = ? AND password = ?", [userid, password], function (err, result, fields) {
       if (err) {
         throw err
-      } else { // id 전체 확인 작업 필요--- result.forEach     result[1]
-        if(result[0].id === post.id && parseInt(result[0].password) === parseInt(post.password)){
-          // cookie insert
-          res.cookie('user', {
-              id: post.id,
-              name: post.password
-          });
-
+      } else { 
+        if(result.length > 0){    // id check
+          // session fill
           res.redirect("/manage");
         } else {
           res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
@@ -109,13 +117,7 @@ app.post('/password_process', function (req, res) {
 
 // 매니지 페이지
 app.get('/manage', (req, res) => {
-  console.log('Cookies: ', req.cookies)
-
-  if(req.headers.cookie != undefined){
     return res.sendFile(__dirname + '/adm/templates/manage.html');
-  }else{
-    res.redirect("/adm");
-  }
 })
 
 // 비밀번호 변경 페이지
