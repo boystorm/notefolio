62b211cb32b919ab353a934a13d24505df07eb7f
boystorm <motokrzr11@gmail.com>
2022-04-03 18:44:17 +0900

    갈길이 멀다

--- controllers/admin/manageController.js
+++ controllers/admin/manageController.js
@@ -1,8 +1,18 @@
 const Manage = require("../../models/admin/manage");
 
 // 카테고리 관리 컨트롤러
-exports.manage = function(req, res){    
-    res.render('admin/manage'); 
+exports.manage = function(req, res){
+    Manage.manageCategory(function(err, result){
+        if(err){
+            res.send(err);
+        } else {
+            console.log(result);
+            res.render('admin/manage', {
+                rows : result
+            }); 
+        }
+    });
+    
 };
 
 // 카테고리 등록 컨트롤러
--- models/admin/manage.js
+++ models/admin/manage.js
@@ -1,8 +1,3 @@
-/**
-* =======================================
-* 설  명 : 데이터베이스 Mysql
-* =======================================
-*/
 const db_config = require('../../config/mysql');
 const conn = db_config.init();
 db_config.connect(conn);
@@ -10,12 +5,14 @@ db_config.connect(conn);
 let Manage = function (){}; // 생성자
 
 // 카테고리 쿼리
-// Author.mainCategory = function(id, password, result){
-//     conn.query("SELECT * FROM author WHERE id = ? AND password = ?", [id, password], function (err, res) {
-//       if(err){
-//         result(null, err);
-//       } else {
-//         result(null, res);
-//       }
-//     });
-//   };
+Manage.manageCategory = function(result){
+    conn.query("SELECT * FROM main_category", function(err, res){
+        if(err){
+            result(null, err);
+        } else {
+            result(null, res);
+        }
+    })
+};
+
+module.exports = Manage;
