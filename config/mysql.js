e728892936296ae9a7444a6460c78e0c0b16aca4
boystorm <motokrzr11@gmail.com>
2022-05-30 23:23:07 +0900

    root 변경

--- config/mysql.js
+++ config/mysql.js
@@ -2,7 +2,7 @@ var mysql = require('mysql2');
 var db_info = {
     host: 'localhost',
     port: '3306',
-    user: 'root',
+    user: 'admin',
     password: '123456',
     database: 'notefolio',
     multipleStatements: true // 다중쿼리 사용    
